f89f0e8c70b76046a7b853571fb1d815c2e8e1c7
lzalit238 <lzalit238@west-mec.org>
2021-09-10 16:43:03 -0700

    Update content.js
    Loading page style and functionality

---
+++ content-page/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './styles/main.css'
+import Component from './Components/content'
+
+function App() {
+  
+  return (
+    <div className="container">
+      <Component />
+    </div>
+  );
+}
+
+export default App;
---
+++ content-page/src/Components/content.js
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from 'react'
+// import { BsArrowRepeat } from "react-icons/bs";
+
+const url = 'https://api.github.com/users/QuincyLarson';
+
+const Content = () => {
+    const [isLoading, setIsLoading] = useState(true)
+    const [isError, setIsError] = useState(false)
+    const [user, setUser] = useState('default user')
+
+    useEffect(() => {
+        fetch(url)
+            .then((response) => response.json())
+            .then((user) => {
+                const { login } = user
+                setUser(login)
+                setIsLoading(false)
+            })
+            .catch((error) => console.log(error))
+    })
+    if (isError) {
+        return (
+            <div>
+                {/* ERROR...(ERROR GIF, ERROR PAGE) */}
+                <h1>Page Not Found</h1>
+                
+            </div>
+        )
+    }
+    if (isLoading) {
+        return (
+            <div>
+                {/* LOADING...(LOADING GIF, LOADING PAGE) */}
+                <h1 className='css_only'>LOADING...</h1>
+            </div>
+        )
+    }
+    return <div>
+    <h1>{user}</h1>
+  </div>;
+}
+export default Content
+
